bd9b643fa6b9576ddc12d2828b83fbd5a0418308
faragon <faragon.github@gmail.com>
2015-06-04 00:09:22 +0200

    cleanup

--- valgrind_check.sh
+++ valgrind_check.sh
@@ -1,2 +1,1 @@
-#make clean; make ; valgrind -v --track-origins=yes --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./stest 2>kk ; less kk
-make clean; make DEBUG=1 PROFILING=1 ; valgrind --track-origins=yes --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./stest 2>&1 | less
+make clean; make DEBUG=1 PROFILING=1 ; valgrind --track-origins=yes --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./stest
