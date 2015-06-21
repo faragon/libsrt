880033393e3010fe9eed814ec228444131d08981
faragon <faragon.github@gmail.com>
2015-06-21 23:58:43 +0200

    libsrt.h added: all-in-one include file

---
+++ src/libsrt.h
@@ -0,0 +1,28 @@
+#ifndef LIBSRT_H
+#define LIBSRT_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * libsrt.h
+ *
+ * Header file for the libsrt library.
+ *
+ * Copyright (c) 2015 F. Aragon. All rights reserved.
+ *
+ */
+
+#include <sstring.h>
+#include <svector.h>
+#include <sbitset.h>
+#include <stree.h>
+#include <smap.h>
+#include <sdmap.h>
+
+#ifdef __cplusplus
+}; /* extern "C" { */
+#endif
+
+#endif /* #ifndef LIBSRT_H */
+
