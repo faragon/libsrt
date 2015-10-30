d1c9f8f7015110a04ab969a92cd258ff3f821de0
faragon <faragon.github@gmail.com>
2015-10-30 23:52:38 +0100

    typo

--- src/sbitset.h
+++ src/sbitset.h
@@ -98,7 +98,7 @@ S_INLINE int sb_test(const sb_t *b, const size_t nth)
 	return (buf[pos] & mask) ? 1 : 0;
 }
 
-/* #API: |Set nth bit to 1|bitset; bit offset|O(1) with the exception of O(n) for first case of not covering unitializated areas -for real-time requirement, force set + clear at last expected writabple position-|1;2| */
+/* #API: |Set nth bit to 1|bitset; bit offset|O(1) with the exception of O(n) for first case of not covering unitializated areas -for real-time requirement, force set + clear at last expected writable position-|1;2| */
 
 S_INLINE void sb_set(sb_t **b, const size_t nth)
 {
