8df74b63e15107ff4f10815b515ebd9ea9571086
faragon <faragon.github@gmail.com>
2015-10-30 23:31:30 +0100

    auto doc

--- doc/mk_doc.sh
+++ doc/mk_doc.sh
@@ -9,6 +9,7 @@
 
 if [ $# != 1 ] ; then
 	echo "Syntax: $0 out_path" >&2
+	echo "Example: $0 ." >&2
 	exit 1
 fi
 
@@ -21,16 +22,23 @@ if [ ! -e "$DOC_PATH_OUT" ] ; then
 fi
 
 DOC_PATH_OUT=$(realpath $DOC_PATH_OUT)
-
 XPWD=$PWD
+INDEX="$DOC_PATH_OUT/libsrt.html"
+
 cd ../src
+echo '<!DOCTYPE html><html><meta http-equiv="Content-Type" content="text/html; charset=latin1"><title>libsrt</title><body>'\
+     '<h1>libsrt documentation </h1><br><br>' > "$INDEX"
 for i in *\.h ; do
 	echo -n "$i: " >&2
 	if $XPWD/c2doc.py "Documentation for $i" < "$i" >"$DOC_PATH_OUT/$i.html" ; then
 		echo "OK" >&2
+		if (( $(wc -l <"$DOC_PATH_OUT/$i.html") > 2 )) ; then
+			echo '<br><a href="'"$i.html"'">'"$i"'</a><br>' >> "$INDEX"
+		fi
 	else	ERRORS=$((ERRORS + 1))
 	fi
 done
+echo '</body></html>' >> "$INDEX"
 cd "$XPWD"
 
 exit $ERRORS
