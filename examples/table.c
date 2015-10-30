1d0b339a0df9964fc27f7f6924c4b03be10e013b
faragon <faragon.github@gmail.com>
2015-10-30 22:55:59 +0100

    refactor

--- examples/table.c
+++ examples/table.c
@@ -5,6 +5,8 @@
  *
  * CSV: http://tools.ietf.org/html/rfc4180
  *
+ * WARNING: incomplete (work in progress)
+ *
  * Copyright (c) 2015 F. Aragon. All rights reserved.
  */
 
@@ -16,53 +18,63 @@
 
 enum EncMode
 {
-	SENC_none,
-	SENC_csv,
-	SENC_xml,
-	SENC_json,
-	SENC_url
+	SENC_start_row,
+	SENC_write_field,
+	SENC_finish_row,
 };
 
-static ssize_t process_row(ss_t *line, ss_t **out, ss_t **tmp, enum EncMode im, enum EncMode om, sbool_t force_flush);
+struct TableCodec;
+
+typedef size_t (*f_dec2x)(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2);
+typedef void (*f_enc)(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out);
+
+struct TableCodec
+{
+	f_dec2x dec2x;
+	f_enc enc;
+};
+
+static size_t csv2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2);
+static size_t xml2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2);
+static size_t json2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2);
+static size_t url2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2);
+static void enc_csv(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out);
+static void enc_xml(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out);
+static void enc_json(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out);
+static void enc_url(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out);
+static ssize_t process_row(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2, sbool_t force_flush);
 
 static int syntax_error(const char **argv, const int exit_code)
 {
 	const char *v0 = argv[0];
-        fprintf(stderr, "Table import/export (libsrt example). Returns: number of "
-                "processed rows\nError [%i] Syntax: %s {-oc|-ox|-oj|-ou}\n"
+        fprintf(stderr, "[INCOMPLETE, work in progress] Table import/export (libsrt example). Returns: number of "
+                "processed rows\nError [%i] Syntax: %s -c{c|x|j|u}{c|x|j|u}\n"
+		"(where: c = csv, x = xml, j = json, u = url)\n"
 		"Examples:\n"
-		"%s -oc <in.csv >out.csv\n%s -ox <in.csv >out.xml\n"
-		"%s -oj <in.csv >out.json\n%s -ou <in.csv >out.url\n",
+		"%s -cc <in.csv >out.csv\n%s -cx <in.csv >out.xml\n"
+		"%s -cj <in.csv >out.json\n%s -cu <in.csv >out.url\n",
 		exit_code, v0, v0, v0, v0, v0);
 	return exit_code;
 }
 
 int main(int argc, const char **argv)
 {
-	if (argc < 2)
+	if (argc < 2 || strlen(argv[1]) != 3 || argv[1][0] != '-')
 		return syntax_error(argv, 1);
 	int exit_code = 0;
 	size_t num_rows = 0;
-	enum EncMode im, om;
-	im = om = SENC_none;
-#if 1
-	im = SENC_csv;
-#endif
-	if (!strncmp(argv[1], "-oc", 3))
-		om = SENC_csv;
-	else if (!strncmp(argv[1], "-ox", 3))
-		om = SENC_xml;
-	else if (!strncmp(argv[1], "-oj", 3))
-		om = SENC_json;
-	else if (!strncmp(argv[1], "-ou", 3))
-		om = SENC_url;
-	if (om == SENC_none)
+	struct TableCodec tcd = { NULL, NULL };
+	tcd.dec2x = argv[1][1] == 'c' ? csv2x : argv[1][1] == 'x' ? xml2x :
+		    argv[1][1] == 'j' ? json2x : argv[1][1] == 'u' ? url2x : NULL;
+	tcd.enc = argv[1][2] == 'c' ? enc_csv : argv[1][2] == 'x' ? enc_xml :
+		  argv[1][2] == 'j' ? enc_json : argv[1][2] == 'u' ? enc_url : NULL;
+	if (!tcd.dec2x || !tcd.enc)
 		return syntax_error(argv, 2);
 	int c = 0;
 	ssize_t l, off, off2;
 	char buf[TABLE_BUF_SIZE];
 	sbool_t new_line = S_FALSE;
-	ss_t *line = NULL, *out = NULL, *tmp = NULL;
+	ss_t *line = NULL, *out = NULL, *tmp = NULL, *tmp2 = NULL;
 	ssize_t j;
 	for (;;) {
 		l = read(0, buf, TABLE_BUF_SIZE);
@@ -80,7 +92,7 @@ int main(int argc, const char **argv)
 			if (off2 == l)
 				break;
 			if (new_line) {
-				if (process_row(line, &out, &tmp, im, om, S_FALSE) < 0) {
+				if (process_row(&tcd, line, &out, &tmp, &tmp2, S_FALSE) < 0) {
 					exit_code = 3;
 					goto main_abort;
 				}
@@ -89,59 +101,124 @@ int main(int argc, const char **argv)
 			off = off2 + 1;
 		}
 	}
-	if (process_row(line, &out, &tmp, im, om, S_TRUE))
+	if (process_row(&tcd, line, &out, &tmp, &tmp2, S_TRUE))
 		exit_code = 3;
 main_abort:
-	ss_free(&line, &tmp, &out);
+	ss_free(&line, &tmp, &tmp2, &out);
 	return exit_code;
 }
 
-static void start_row(ss_t **out, enum EncMode om)
+static void enc_csv(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out)
 {
-	switch (om) {
-	case SENC_csv:	break;
-	case SENC_xml: 	ss_cat_c(out, "<row type=\"array\">"); break;
-	case SENC_json:	ss_cat_c(out, "["); break;
-	case SENC_url:	break;
-	default:	break;
-	}
-}
-
-static void write_field(size_t nfield, ss_t **field, ss_t **out, enum EncMode om)
-{
-#ifdef DBG_TABLE
-	fprintf(stderr, "write_field: '%s'\n", ss_to_c(*field));
-#endif
-	switch (om) {
-	case SENC_csv:
-		if (nfield > 0)
-			ss_cat_cn(out, ",", 1);
-		ss_cat_cn(out, "\"", 1);
-		ss_cat_enc_esc_dquote(out, ss_trim(field));
-		ss_cat_cn(out, "\"", 1);
+	switch (em) {
+	case SENC_start_row:
 		break;
-	case SENC_xml: 	break;
-	case SENC_json:	break;
-	case SENC_url:	break;
-	default:	break;
+	case SENC_write_field:
+#ifdef DBG_TABLE
+		fprintf(stderr, "enc_csv write_field: '%s', '%s'\n", ss_to_c(*field_l), ss_to_c(*field_r));
+#endif
+		if (nfields > 2) /* Not the first two pairs */
+			ss_cat_cn(out, ",\"", 2);
+		else
+			ss_cat_cn(out, "\"", 1);
+		ss_cat_enc_esc_dquote(out, ss_trim(field_l));
+		if ((nfields % 2) == 0) { /* pair */
+			ss_cat_cn(out, "\",\"", 3);
+			ss_cat_enc_esc_dquote(out, ss_trim(field_r));
+			ss_cat_cn(out, "\"", 1);
+		} else { /* single field */
+			ss_cat_cn(out, "\"", 1);
+		}
+		break;
+	case SENC_finish_row:
+		ss_cat_cn(out, "\n", 1);
+		break;
+	default:
+		break;
 	}
-	ss_set_size(*field, 0);
 }
 
-static void finish_row(ss_t **out, enum EncMode om)
+static void enc_xml(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out)
 {
-	switch (om) {
-	case SENC_csv:	break;
-	case SENC_xml: 	ss_cat_c(out, "</row>"); break;
-	case SENC_json:	ss_cat_c(out, "]"); break;
-	case SENC_url:	break;
-	default:	break;
+	switch (em) {
+	case SENC_start_row:
+		ss_cat_c(out, "<row type=\"array\">");
+		break;
+	case SENC_write_field:
+#ifdef DBG_TABLE
+		fprintf(stderr, "enc_xml write_field: '%s', '%s'\n", ss_to_c(*field_l), ss_to_c(*field_r));
+#endif
+		ss_cat_cn(out, "<", 1);
+		ss_cat_enc_esc_xml(out, ss_trim(field_l));
+		ss_cat_cn(out, ">", 1);
+		if ((nfields % 2) == 0)
+			ss_cat_enc_esc_xml(out, ss_trim(field_r));
+		ss_cat_cn(out, "</", 2);
+		ss_cat(out, *field_l);
+		ss_cat_cn(out, ">", 1);
+		break;
+	case SENC_finish_row:
+		ss_cat_cn(out, "</row>\n", 7);
+		break;
+	default:
+		break;
 	}
 }
 
-static sbool_t csv2x(ss_t *line, ss_t **out, ss_t **tmp, enum EncMode om)
+static void enc_json(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out)
 {
-	sbool_t res_ok = S_TRUE;
+	switch (em) {
+	case SENC_start_row:
+		ss_cat_c(out, "[");
+		break;
+	case SENC_write_field:
+#ifdef DBG_TABLE
+		fprintf(stderr, "enc_json write_field: '%s', '%s'\n", ss_to_c(*field_l), ss_to_c(*field_r));
+#endif
+		if (nfields > 2) /* Not the first two pairs */
+			ss_cat_cn(out, ",\"", 2);
+		else
+			ss_cat_cn(out, "\"", 1);
+		ss_cat_enc_esc_json(out, ss_trim(field_l));
+		ss_cat_cn(out, "\":\"", 3);
+		if ((nfields % 2) == 0)
+			ss_cat_enc_esc_json(out, ss_trim(field_r));
+		ss_cat_cn(out, "\"", 1);
+
+		break;
+	case SENC_finish_row:
+		ss_cat_cn(out, "]\n", 2);
+		break;
+	default:
+		break;
+	}
+}
+
+static void enc_url(enum EncMode em, size_t nfields, ss_t **field_l, ss_t **field_r, ss_t **out)
+{
+	switch (em) {
+	case SENC_start_row:
+		break;
+	case SENC_write_field:
+#ifdef DBG_TABLE
+		fprintf(stderr, "enc_url write_field: '%s', '%s'\n", ss_to_c(*field_l), ss_to_c(*field_r));
+#endif
+		ss_cat_cn(out, nfields > 2 ? "&" : "?", 1);
+		ss_cat_enc_esc_url(out, ss_trim(field_l));
+		ss_cat_cn(out, "=", 1);
+		if ((nfields % 2) == 0)
+			ss_cat_enc_esc_url(out, ss_trim(field_r));
+		break;
+	case SENC_finish_row:
+		ss_cat_cn(out, "\n", 1);
+		break;
+	default:
+		break;
+	}
+}
+
+static size_t csv2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2)
+{
 	size_t off = 0, nfield = 0, qo, co, qo2;
 	ss_t *tq = ss_alloca(1), *tc = ss_alloca(1);
 	ss_cpy_cn(&tq, "\"", 1);
@@ -150,81 +227,83 @@ static sbool_t csv2x(ss_t *line, ss_t **out, ss_t **tmp, enum EncMode om)
 	size_t ls = ss_size(line);
 	size_t out_off0 = ss_size(*out);
 	ss_set_size(*tmp, 0);
+	ss_set_size(*tmp2, 0);
+	ss_t ***field = &tmp;
 	for (; off < ls;) {
 		qo = ss_find(line, off, tq);
 		co = ss_find(line, off, tc);
 		if (co < qo) { /* No double quote */
-			ss_cat_cn(tmp, l + off, co - off);
-			write_field(nfield++, tmp, out, om);
+			ss_cat_cn(*field, l + off, co - off);
+			if ((nfield++ % 2) != 0) {
+				tcd->enc(SENC_write_field, nfield, tmp, tmp2, out);
+				ss_set_size(*tmp, 0);
+				ss_set_size(*tmp2, 0);
+				field = &tmp;
+			} else {
+				field = &tmp2;
+			}
 			off = s_size_t_add(co, 1, S_NPOS);
 		} else if (qo < co) { /* Double quote start */
-			ss_cat_cn(tmp, l + off, qo - off);
+			ss_cat_cn(*field, l + off, qo - off);
 			off = s_size_t_add(qo, 1, S_NPOS);
 			for (; off < ls;) {
 				qo2 = ss_find(line, off, tq);
 				if (qo2 == off) { /* Two "" */
-					ss_cat_cn(tmp, l + off, 1);
+					ss_cat_cn(*field, l + off, 1);
 					off = s_size_t_add(qo2, 1, S_NPOS);
 					break;
 				} else if (qo2 == S_NPOS) { /* BEHAVIOR: underflow */
-					ss_cat_cn(tmp, l + off, ls - off);
+					ss_cat_cn(*field, l + off, ls - off);
 					off = ls;
 					break;
 				} else {
 					/* 1) "...""..." vs 2) "..." */
 					size_t inc = qo2 + 2 < ls &&
 						     l[qo2 + 1] == '\"' ? 1 : 0;
-					ss_cat_cn(tmp, l + off, qo2 + inc - off);
+					ss_cat_cn(*field, l + off, qo2 + inc - off);
 					off = s_size_t_add(qo2, 1 + inc, S_NPOS);
 					if (inc == 0) /* case (2) */
 						break;
 				}
 			}
 		} else {
-			ss_cat_cn(tmp, l + off, ls - off);
+			ss_cat_cn(*field, l + off, ls - off);
 			/*off = ls;*/
 			break;
 		}
 	}
-	if (ss_size(*tmp) > 0)
-		write_field(nfield++, tmp, out, om);
-	ss_cat_cn(out, "\n", 1);
-	return res_ok;
+	if (ss_size(**field) > 0)
+		tcd->enc(SENC_write_field, ++nfield, tmp, tmp2, out);
+	return nfield;
 }
 
-static sbool_t xml2x(ss_t *line, ss_t **out, ss_t **tmp, enum EncMode om)
+static size_t xml2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2)
 {
-	return S_FALSE;
+	return 0;
 }
 
-static sbool_t json2x(ss_t *line, ss_t **out, ss_t **tmp, enum EncMode om)
+static size_t json2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2)
 {
-	return S_FALSE;
+	return 0;
 }
 
-static sbool_t url2x(ss_t *line, ss_t **out, ss_t **tmp, enum EncMode om)
+static size_t url2x(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2)
 {
-	return S_FALSE;
+	return 0;
 }
 
-static ssize_t process_row(ss_t *line, ss_t **out, ss_t **tmp, enum EncMode im, enum EncMode om, sbool_t force_flush)
+static ssize_t process_row(const struct TableCodec *tcd, ss_t *line, ss_t **out, ss_t **tmp, ss_t **tmp2, sbool_t force_flush)
 {
-	ssize_t written = 0;
-#if 1 /* TODO: work in progress, this is just a temporary echo test */
+#ifdef DBG_TABLE
 	fprintf(stderr, "line: '%s'\n", ss_to_c(line));
-	start_row(out, om);
-	switch (im) {
-	case SENC_csv: csv2x(line, out, tmp, om); break;
-	case SENC_xml: xml2x(line, out, tmp, om); break;
-	case SENC_json: json2x(line, out, tmp, om); break;
-	case SENC_url: url2x(line, out, tmp, om); break;
-	default:
-		break;
-	}
-	finish_row(out, om);
-#else
-	ss_cat(out, line);
 #endif
+	size_t written = 0, out_size_rollback = ss_size(*out);
+	tcd->enc(SENC_start_row, 0, NULL, NULL, out);
+	if (tcd->dec2x(tcd, line, out, tmp, tmp2)) {
+		tcd->enc(SENC_finish_row, 0, NULL, NULL, out);
+	} else {
+		ss_set_size(*out, out_size_rollback);
+	}
 	size_t sso = ss_size(*out);
 	if (sso > TABLE_BUF_SIZE || (sso > 0 && force_flush)) {
 		written = write(1, ss_to_c(*out), ss_size(*out));
