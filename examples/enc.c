fb16c68b0bc2e57414c204a0099aeee2750d5eec
faragon <faragon.github@gmail.com>
2016-10-30 22:41:30 +0100

    'enc' example simplified, using only the API and not internal functions

--- examples/enc.c
+++ examples/enc.c
@@ -8,27 +8,6 @@
  */
 
 #include "../src/libsrt.h"
-#include "../src/saux/senc.h"
-
-enum EncMode
-{
-	SENC_none,
-	SENC_b64,
-	SDEC_b64,
-	SENC_hex,
-	SENC_HEX,
-	SDEC_hex,
-	SENC_xml,
-	SDEC_xml,
-	SENC_json,
-	SDEC_json,
-	SENC_url,
-	SDEC_url,
-	SENC_lzw,
-	SDEC_lzw,
-	SENC_rle,
-	SDEC_rle
-};
 
 #define IBUF_SIZE	(3 * 4 * 1024) /* 3 * 4: LCM */
 #define OBUF_SIZE	(IBUF_SIZE * 6) /* Max req: xml escape */
@@ -40,14 +19,26 @@ static int syntax_error(const char **argv, const int exit_code)
 	const char *v0 = argv[0];
 	fprintf(stderr,
 		"Error [%i] Syntax: %s [-eb|-db|-eh|-eH|-dh|-ex|-dx|-ej|-dj|"
-		"-eu|-du|-ez|-dz]\nExamples:\n"
+#if 0 /* will be added once LZW/RLE is added properly to the API */
+		"-eu|-du|-er|-dr|-ez|-dz]\nExamples:\n"
+#else
+		"-eu|-du]\nExamples:\n"
+#endif
 		"%s -eb <in >out.b64\n%s -db <in.b64 >out\n"
 		"%s -eh <in >out.hex\n%s -eH <in >out.HEX\n"
 		"%s -dh <in.hex >out\n%s -dh <in.HEX >out\n"
 		"%s -ex <in >out.xml.esc\n%s -dx <in.xml.esc >out\n"
 		"%s -ej <in >out.json.esc\n%s -dx <in.json.esc >out\n"
+		"%s -eu <in >out.url.esc\n%s -du <in.url.esc >out\n"
+#if 0 /* will be added once LZW/RLE is added properly to the API */
+		, "%s -er <in >in.r\n%s -dr <in.r >out\n"
 		"%s -ez <in >in.z\n%s -dz <in.z >out\n",
-		exit_code, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0);
+		exit_code, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0,
+		v0, v0, v0, v0, v0);
+#else
+		, exit_code, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0, v0,
+		v0);
+#endif
 	return exit_code;
 }
 
@@ -55,179 +46,142 @@ int main(int argc, const char **argv)
 {
 	if (argc < 2)
 		return syntax_error(argv, 1);
-	enum EncMode mode = SENC_none;
+	ss_t *in = ss_alloc(IBUF_SIZE * 2), *out = ss_alloc(IBUF_SIZE * 2);
+	ss_t *(*ss_codec1_f)(ss_t **s, const ss_t *src) = NULL;
+	ss_t *(*ss_codec2_f)(ss_t **s, const ss_t *src) = NULL;
+	ss_t *(*ss_codec3_f)(ss_t **s, const ss_t *src) = NULL;
+	ss_t *(*ss_codec4_f)(ss_t **s, const ss_t *src) = NULL;
 	if (!strncmp(argv[1], "-eb", 3))
-		mode = SENC_b64;
+		ss_codec1_f = ss_enc_b64;
 	else if (!strncmp(argv[1], "-db", 3))
-		mode = SDEC_b64;
+		ss_codec1_f = ss_dec_b64;
 	else if (!strncmp(argv[1], "-eh", 3))
-		mode = SENC_hex;
+		ss_codec1_f = ss_enc_hex;
 	else if (!strncmp(argv[1], "-eH", 3))
-		mode = SENC_HEX;
+		ss_codec1_f = ss_enc_HEX;
 	else if (!strncmp(argv[1], "-dh", 3))
-		mode = SDEC_hex;
+		ss_codec1_f = ss_dec_hex;
 	else if (!strncmp(argv[1], "-ex", 3))
-		mode = SENC_xml;
+		ss_codec1_f = ss_enc_esc_xml;
 	else if (!strncmp(argv[1], "-dx", 3))
-		mode = SDEC_xml;
+		ss_codec2_f = ss_dec_esc_xml;
 	else if (!strncmp(argv[1], "-ej", 3))
-		mode = SENC_json;
+		ss_codec1_f = ss_enc_esc_json;
 	else if (!strncmp(argv[1], "-dj", 3))
-		mode = SDEC_json;
+		ss_codec2_f = ss_dec_esc_json;
 	else if (!strncmp(argv[1], "-eu", 3))
-		mode = SENC_url;
+		ss_codec1_f = ss_enc_esc_url;
 	else if (!strncmp(argv[1], "-du", 3))
-		mode = SDEC_url;
+		ss_codec2_f = ss_dec_esc_url;
+#if 0 /* will be added once LZW/RLE is added properly to the API */
 	else if (!strncmp(argv[1], "-ez", 3))
-		mode = SENC_lzw;
+		ss_codec3_f = ss_enc_lzw;
 	else if (!strncmp(argv[1], "-dz", 3))
-		mode = SDEC_lzw;
+		ss_codec4_f = ss_dec_lzw;
 	else if (!strncmp(argv[1], "-er", 3))
-		mode = SENC_rle;
+		ss_codec3_f = ss_enc_rle;
 	else if (!strncmp(argv[1], "-dr", 3))
-		mode = SDEC_rle;
-	if (mode == SENC_none)
+		ss_codec4_f = ss_dec_rle;
+#endif
+	else
 		return syntax_error(argv, 2);
-	unsigned l32;
-	size_t j, l, li = 0, lo = 0, lo2, off = 0;
-	size_t is; /* input elem size */
-	unsigned char buf[XBUF_SIZE], bufo[XBUF_SIZE], esc;
 	int exit_code = 0;
-	switch (mode) {
-	case SENC_b64: is = 3; break;
-	case SDEC_b64: is = 4; break;
-	case SENC_hex:
-	case SENC_HEX: is = 1; break;
-	case SDEC_hex: is = 2; break;
-	case SENC_xml:
-	case SDEC_xml:
-	case SENC_json:
-	case SDEC_json:
-	case SENC_url:
-	case SDEC_url: is = 1; break;
-	default: is = 0;
-	}
+	unsigned char esc = ss_codec2_f == ss_dec_esc_xml ? '&' :
+			    ss_codec2_f == ss_dec_esc_json ? '\\' : '%';
+	ss_ref_t ref;
+	size_t li = 0, lo = 0, j;
+	if (ss_codec1_f || ss_codec2_f || ss_codec3_f || ss_codec4_f)
 	for (;;) {
-		if (is) {
-			l = fread(buf + off, 1, IBUF_SIZE, stdin);
-			if ((!l && !off) || ferror(stdin))
+		if (ss_codec1_f || ss_codec2_f) {
+			size_t ss0 = ss_size(in);
+			ss_cat_read(&in, stdin, IBUF_SIZE);
+			size_t l = ss_size(in);
+			li += (l - ss0);
+			if (!l || ferror(stdin))
 				goto done;
-			l += off;
-			li += l;
-			switch (mode) {
-			case SENC_b64:
-				lo2 = senc_b64(buf, (size_t)l, bufo);
-				break;
-			case SDEC_b64:
-				lo2 = sdec_b64(buf, (size_t)l, bufo);
-				break;
-			case SENC_hex:
-				lo2 = senc_hex(buf, (size_t)l, bufo);
-				break;
-			case SENC_HEX:
-				lo2 = senc_HEX(buf, (size_t)l, bufo);
-				break;
-			case SDEC_hex:
-				lo2 = sdec_hex(buf, (size_t)l, bufo);
-				break;
-			case SENC_xml:
-				lo2 = senc_esc_xml(buf, (size_t)l, bufo, 0);
-				break;
-			case SENC_json:
-				lo2 = senc_esc_json(buf, (size_t)l, bufo, 0);
-				break;
-			case SENC_url:
-				lo2 = senc_esc_url(buf, (size_t)l, bufo, 0);
-				break;
-			case SDEC_xml:
-			case SDEC_json:
-			case SDEC_url:
-				off = 0;
-				esc = mode == SDEC_xml ? '&' :
-				      mode == SDEC_json ? '\\' : '%';
+			if (ss_codec1_f) {
+				ss_codec1_f(&out, in);
+				ss_clear(in);
+			} else {
+				const char *b = ss_get_buffer_r(in);
+				size_t off = 0;
 				if (l > ESC_MAX_SIZE)
 					for (j = (size_t)(l - ESC_MAX_SIZE + 1);
 					     j < l; j++)
-						if (buf[j] == esc && buf[j - 1] != esc) {
+						if (b[j] == esc &&
+						    b[j - 1] != esc) {
 							off = (size_t)(l - j);
 							break;
 						}
-				if (l > off)
-					l -= off;
-				else
-					l = 0;
-				lo2 = mode == SDEC_xml ?
-					sdec_esc_xml(buf, l, bufo) :
-				      mode == SDEC_json ?
-					sdec_esc_json(buf, l, bufo) :
-					sdec_esc_url(buf, l, bufo);
+				l -= off;
+				ss_codec2_f(&out, ss_ref_buf(&ref, b, l));
 				if (off > 0)
-					memcpy(buf, buf + l, off);
-				break;
-			default:
-				exit_code = 1;
-				goto done;
+					ss_cpy_substr(&in, in, l, S_NPOS);
+				else
+					ss_clear(in);
 			}
 		} else {
-			switch (mode) {
-			case SENC_lzw:
-			case SENC_rle:
-				l = fread(buf, 1, IBUF_SIZE, stdin);
-				if (!l || ferror(stdin))
+#if 0 /* will be added once LZW/RLE is added properly to the API */
+			uint32_t l32 = 0;
+			if (ss_codec3_f) { /* lzw/rle encoding */
+				ss_cpy_read(&in, stdin, IBUF_SIZE);
+				size_t l = ss_size(in);
+				if (!l)
 					goto done;
-				li += (size_t)l;
-				lo2 = mode == SENC_lzw ?
-					senc_lzw(buf, l, bufo + 4) :
-					senc_rle(buf, l, bufo + 4);
-				l32 = (unsigned)lo2;
+				li += l;
+				ss_codec3_f(&out, in);
+				l32 = (uint32_t)ss_size(out);
 				l32 = S_HTON_U32(l32);
-				S_ST_U32(bufo, l32);
-				lo2 += 4;
-				break;
-			case SDEC_lzw:
-			case SDEC_rle:
-				l = fread(&l32, 1, 4, stdin);
+				if (!fwrite(&l32, 1, 4, stdout) ||
+				    ferror(stdout)) {
+					fprintf(stderr, "Write error\n");
+					exit_code = 6;
+					goto done;
+				}
+				lo += 4;
+				ss_clear(in);
+			} else { /* lzw/rle decoding */
+				size_t l = fread(&l32, 1, 4, stdin);
 				if (!l || ferror(stdin))
 					goto done;
+				li += l;
 				if (l != 4) {
 					exit_code = 2;
 					fprintf(stderr, "Format error\n");
 					goto done;
 				}
-				li += (size_t)l;
 				l32 = S_NTOH_U32(l32);
 				if (l32 > XBUF_SIZE) {
 					fprintf(stderr, "Format error\n");
 					exit_code = 3;
 					goto done;
 				}
-				l = fread(buf, 1, l32, stdin);
-				if ((unsigned)l != l32 || ferror(stdin)) {
+				ss_cpy_read(&in, stdin, l32);
+				l = ss_size(in);
+				li += l;
+				if (l != l32 || ferror(stdin)) {
 					fprintf(stderr, "Read error\n");
 					exit_code = 4;
 					goto done;
 				}
-				li += (size_t)l;
-				lo2 = mode == SDEC_lzw ?
-					sdec_lzw(buf, l, bufo) :
-					sdec_rle(buf, l, bufo);
-				break;
-			default:
-				exit_code = 5;
-				goto done;
+				ss_codec4_f(&out, in);
 			}
+#else
+			break;
+#endif
 		}
-		l = fwrite(bufo, 1, lo2, stdout);
-		if (!l || ferror(stdout)) {
-			fprintf(stderr, "Write error (%i)\n", (int)l);
+		if (ss_write(stdout, out, 0, S_NPOS) < 0) {
+			fprintf(stderr, "Write error\n");
 			exit_code = 6;
 			goto done;
 		}
-		lo += lo2;
+		lo += ss_size(out);
+		ss_clear(out);
 	}
 done:
 	fprintf(stderr, "in: %u bytes, out: %u bytes\n",
 		(unsigned)li, (unsigned)lo);
+	ss_free(&in, &out);
 	return exit_code;
 }
 
