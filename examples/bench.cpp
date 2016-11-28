88c2055dadbc0103a3af4a8d7cf8d2934770da43
faragon <faragon.github@gmail.com>
2016-11-28 01:07:17 +0100

    benchmarks

--- examples/bench.cpp
+++ examples/bench.cpp
@@ -21,54 +21,66 @@
 #define BENCH_INIT		\
 	struct timespec ta, tb
 #define BENCH_TIME_US				\
-	((tb.tv_sec - ta.tv_sec) * 1000000 + (tb.tv_nsec - ta.tv_nsec) / 1000)
-#define BENCH_FN(test_fn, count)					\
-	clock_gettime(CLOCK_REALTIME, &ta);				\
-	test_fn(count);							\
-	clock_gettime(CLOCK_REALTIME, &tb);				\
-	printf("test %s: %u.%03u seconds (%u elements)\n", #test_fn,	\
-	       (unsigned)(BENCH_TIME_MS / 1000),			\
-	       (unsigned)(BENCH_TIME_MS % 1000),			\
-	       (unsigned)count)
+	((tb.tv_sec - ta.tv_sec) * 1000000 +	\
+	 (tb.tv_nsec - ta.tv_nsec) / 1000)
+#define BENCH_FN(test_fn, count, nread)			\
+	clock_gettime(CLOCK_REALTIME, &ta);		\
+	test_fn(count, nread);				\
+	clock_gettime(CLOCK_REALTIME, &tb);		\
+	printf("| %s | %zu | %zu | - | %u.%03u |\n",	\
+	       #test_fn, count, count * nread,		\
+	       (unsigned)(BENCH_TIME_MS / 1000),	\
+	       (unsigned)(BENCH_TIME_MS % 1000))
 #else
 #define BENCH_INIT
-#define BENCH_FN
+#define BENCH_FN(test_fn, count, nread)
 #define BENCH_TIME_US 0
 #endif
-#define BENCH_TIME_MS		\
-	(BENCH_TIME_US / 1000)
+#define BENCH_TIME_MS (BENCH_TIME_US / 1000)
 
-void ctest_map_ii32(size_t count)
+void ctest_map_ii32(size_t count, size_t read_ntimes)
 {
 	sm_t *m = sm_alloc(SM_II32, 0);
 	for (size_t i = 0; i < count; i++)
 		sm_insert_ii32(&m, (int32_t)i, (int32_t)i);
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)sm_at_ii32(m, (int32_t)i);
 	sm_free(&m);
 }
 
-void cxxtest_map_ii32(size_t count)
+void cxxtest_map_ii32(size_t count, size_t read_ntimes)
 {
 	std::map <int32_t, int32_t> m;
 	for (size_t i = 0; i < count; i++)
 		m[i] = (int32_t)i;
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)m[i];
 }
 
-void ctest_map_ii64(size_t count)
+void ctest_map_ii64(size_t count, size_t read_ntimes)
 {
 	sm_t *m = sm_alloc(SM_II, 0);
 	for (size_t i = 0; i < count; i++)
 		sm_insert_ii(&m, (int64_t)i, (int64_t)i);
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)sm_at_ii(m, (int64_t)i);
 	sm_free(&m);
 }
 
-void cxxtest_map_ii64(size_t count)
+void cxxtest_map_ii64(size_t count, size_t read_ntimes)
 {
 	std::map <int64_t, int64_t> m;
 	for (size_t i = 0; i < count; i++)
 		m[i] = (int64_t)i;
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)m[i];
 }
 
-void ctest_map_s16(size_t count)
+void ctest_map_s16(size_t count, size_t read_ntimes)
 {
 	ss_t *btmp = ss_alloca(512);
 	sm_t *m = sm_alloc(SM_SS, 0);
@@ -76,10 +88,15 @@ void ctest_map_s16(size_t count)
 		ss_printf(&btmp, 512, "%016i", (int)i);
 		sm_insert_ss(&m, btmp, btmp);
 	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			ss_printf(&btmp, 512, "%016i", (int)i);
+			(void)sm_at_ss(m, btmp);
+		}
 	sm_free(&m);
 }
 
-void cxxtest_map_s16(size_t count)
+void cxxtest_map_s16(size_t count, size_t read_ntimes)
 {
 	char btmp[512];
 	std::map <std::string, std::string> m;
@@ -87,9 +104,14 @@ void cxxtest_map_s16(size_t count)
 		sprintf(btmp, "%016i", (int)i);
 		m[btmp] = btmp;
 	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			sprintf(btmp, "%016i", (int)i);
+			(void)m[btmp];
+		}
 }
 
-void ctest_map_s64(size_t count)
+void ctest_map_s64(size_t count, size_t read_ntimes)
 {
 	ss_t *btmp = ss_alloca(512);
 	sm_t *m = sm_alloc(SM_SS, 0);
@@ -97,10 +119,15 @@ void ctest_map_s64(size_t count)
 		ss_printf(&btmp, 512, "%064i", (int)i);
 		sm_insert_ss(&m, btmp, btmp);
 	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			ss_printf(&btmp, 512, "%064i", (int)i);
+			(void)sm_at_ss(m, btmp);
+		}
 	sm_free(&m);
 }
 
-void cxxtest_map_s64(size_t count)
+void cxxtest_map_s64(size_t count, size_t read_ntimes)
 {
 	char btmp[512];
 	std::map <std::string, std::string> m;
@@ -108,25 +135,36 @@ void cxxtest_map_s64(size_t count)
 		sprintf(btmp, "%064i", (int)i);
 		m[btmp] = btmp;
 	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			sprintf(btmp, "%064i", (int)i);
+			(void)m[btmp];
+		}
 }
 
 #if __cplusplus >= 201103L
 
-void cxxtest_umap_ii32(size_t count)
+void cxxtest_umap_ii32(size_t count, size_t read_ntimes)
 {
 	std::unordered_map <int32_t, int32_t> m;
 	for (size_t i = 0; i < count; i++)
 		m[i] = (int32_t)i;
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)m.count(i);
 }
 
-void cxxtest_umap_ii64(size_t count)
+void cxxtest_umap_ii64(size_t count, size_t read_ntimes)
 {
 	std::unordered_map <int64_t, int64_t> m;
 	for (size_t i = 0; i < count; i++)
 		m[i] = (int64_t)i;
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)m.count(i);
 }
 
-void cxxtest_umap_s16(size_t count)
+void cxxtest_umap_s16(size_t count, size_t read_ntimes)
 {
 	char btmp[512];
 	std::unordered_map <std::string, std::string> m;
@@ -134,9 +172,14 @@ void cxxtest_umap_s16(size_t count)
 		sprintf(btmp, "%016i", (int)i);
 		m[btmp] = btmp;
 	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			sprintf(btmp, "%016i", (int)i);
+			(void)m.count(btmp);
+		}
 }
 
-void cxxtest_umap_s64(size_t count)
+void cxxtest_umap_s64(size_t count, size_t read_ntimes)
 {
 	char btmp[512];
 	std::unordered_map <std::string, std::string> m;
@@ -144,35 +187,164 @@ void cxxtest_umap_s64(size_t count)
 		sprintf(btmp, "%064i", (int)i);
 		m[btmp] = btmp;
 	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			sprintf(btmp, "%064i", (int)i);
+			(void)m.count(btmp);
+		}
 }
 
 #endif
 
+void ctest_set_i32(size_t count, size_t read_ntimes)
+{
+	sms_t *m = sms_alloc(SMS_I32, 0);
+	for (size_t i = 0; i < count; i++)
+		sms_insert_i32(&m, (int32_t)i);
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)sms_count_i(m, (int32_t)i);
+	sms_free(&m);
+}
+
+void cxxtest_set_i32(size_t count, size_t read_ntimes)
+{
+	std::set <int32_t> m;
+	for (size_t i = 0; i < count; i++)
+		m.insert((int32_t)i);
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)m.count(i);
+}
+
+void ctest_set_i64(size_t count, size_t read_ntimes)
+{
+	sms_t *m = sms_alloc(SMS_I, 0);
+	for (size_t i = 0; i < count; i++)
+		sms_insert_i32(&m, (int64_t)i);
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)sms_count_i(m, (int64_t)i);
+	sms_free(&m);
+}
+
+void cxxtest_set_i64(size_t count, size_t read_ntimes)
+{
+	std::set <int64_t> m;
+	for (size_t i = 0; i < count; i++)
+		m.insert((int64_t)i);
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++)
+			(void)m.count(i);
+}
+
+void ctest_set_s16(size_t count, size_t read_ntimes)
+{
+	ss_t *btmp = ss_alloca(512);
+	sms_t *m = sms_alloc(SMS_S, 0);
+	for (size_t i = 0; i < count; i++) {
+		ss_printf(&btmp, 512, "%016i", (int)i);
+		sms_insert_s(&m, btmp);
+	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			ss_printf(&btmp, 512, "%016i", (int)i);
+			(void)sms_count_s(m, btmp);
+		}
+	sms_free(&m);
+}
+
+void cxxtest_set_s16(size_t count, size_t read_ntimes)
+{
+	char btmp[512];
+	std::set <std::string> m;
+	for (size_t i = 0; i < count; i++) {
+		sprintf(btmp, "%016i", (int)i);
+		m.insert(btmp);
+	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			sprintf(btmp, "%016i", (int)i);
+			(void)m.count(btmp);
+		}
+}
+
+void ctest_set_s64(size_t count, size_t read_ntimes)
+{
+	ss_t *btmp = ss_alloca(512);
+	sms_t *m = sms_alloc(SMS_S, 0);
+	for (size_t i = 0; i < count; i++) {
+		ss_printf(&btmp, 512, "%064i", (int)i);
+		sms_insert_s(&m, btmp);
+	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			ss_printf(&btmp, 512, "%064i", (int)i);
+			(void)sms_count_s(m, btmp);
+		}
+
+	sms_free(&m);
+}
+
+void cxxtest_set_s64(size_t count, size_t read_ntimes)
+{
+	char btmp[512];
+	std::set <std::string> m;
+	for (size_t i = 0; i < count; i++) {
+		sprintf(btmp, "%064i", (int)i);
+		m.insert(btmp);
+	}
+	for (size_t j = 0; j < read_ntimes; j++)
+		for (size_t i = 0; i < count; i++) {
+			sprintf(btmp, "%064i", (int)i);
+			(void)m.count(btmp);
+		}
+}
+
 int main(int argc, char *argv[])
 {
 	BENCH_INIT;
-	size_t count = 1000000;
-	printf("Insert %zu elements, and then, release memory.\n", count);
-	BENCH_FN(ctest_map_ii32, count);
-	BENCH_FN(cxxtest_map_ii32, count);
+	const size_t ntests = 2;
+	size_t count[ntests] = { 1000000, 1000000 };
+	size_t nread[ntests] = { 0, 10 };
+	char label[ntests][512];
+	snprintf(label[0], 512, "Insert %zu elements, cleanup", count[0]);
+	snprintf(label[1], 512, "Insert %zu elements, read all elements %zu "
+		 "times, cleanup", count[1], nread[1]);
+	for (size_t i = 0; i < ntests; i++) {
+		printf("\n%s\n"
+		       "| Test | Insert count | Read count | Memory (MB) | Execution time (s) |\n"
+		       "|:---:|:---:|:---:|:---:|:---:|\n",
+		       label[i]);
+		BENCH_FN(ctest_map_ii32, count[i], nread[i]);
+		BENCH_FN(cxxtest_map_ii32, count[i], nread[i]);
 #if __cplusplus >= 201103L
-	BENCH_FN(cxxtest_umap_ii32, count);
+		BENCH_FN(cxxtest_umap_ii32, count[i], nread[i]);
 #endif
-	BENCH_FN(ctest_map_ii64, count);
-	BENCH_FN(cxxtest_map_ii64, count);
+		BENCH_FN(ctest_map_ii64, count[i], nread[i]);
+		BENCH_FN(cxxtest_map_ii64, count[i], nread[i]);
 #if __cplusplus >= 201103L
-	BENCH_FN(cxxtest_umap_ii64, count);
+		BENCH_FN(cxxtest_umap_ii64, count[i], nread[i]);
 #endif
-	BENCH_FN(ctest_map_s16, count);
-	BENCH_FN(cxxtest_map_s16, count);
+		BENCH_FN(ctest_map_s16, count[i], nread[i]);
+		BENCH_FN(cxxtest_map_s16, count[i], nread[i]);
 #if __cplusplus >= 201103L
-	BENCH_FN(cxxtest_umap_s16, count);
+		BENCH_FN(cxxtest_umap_s16, count[i], nread[i]);
 #endif
-	BENCH_FN(ctest_map_s64, count);
-	BENCH_FN(cxxtest_map_s64, count);
+		BENCH_FN(ctest_map_s64, count[i], nread[i]);
+		BENCH_FN(cxxtest_map_s64, count[i], nread[i]);
 #if __cplusplus >= 201103L
-	BENCH_FN(cxxtest_umap_s64, count);
+		BENCH_FN(cxxtest_umap_s64, count[i], nread[i]);
 #endif
+		BENCH_FN(ctest_set_i32, count[i], nread[i]);
+		BENCH_FN(cxxtest_set_i32, count[i], nread[i]);
+		BENCH_FN(ctest_set_i64, count[i], nread[i]);
+		BENCH_FN(cxxtest_set_i64, count[i], nread[i]);
+		BENCH_FN(ctest_set_s16, count[i], nread[i]);
+		BENCH_FN(cxxtest_set_s16, count[i], nread[i]);
+		BENCH_FN(ctest_set_s64, count[i], nread[i]);
+		BENCH_FN(cxxtest_set_s64, count[i], nread[i]);
+	}
 	return 0;
 }
 
