a28678af846ac786e405344db6a9ea2d2655fc48
faragon <faragon.github@gmail.com>
2018-02-14 23:30:27 +0100

    new Valgrind test covering realloc() edge cases

--- src/saux/sconfig.h
+++ src/saux/sconfig.h
@@ -30,6 +30,30 @@ extern "C" {
 	#define _s_free free
 #endif
 
+/*
+ * For covering the case of errors masked because of the realloc function
+ * returning the same base address, a mechanism for detecting those errors
+ * is provided (used in the Valgrind test phase, protecting the build)
+ */
+#if defined(S_FORCE_REALLOC_COPY) && defined(_s_realloc) &&		\
+    (defined(__GNUC__) || defined(__clang__) || defined(__TINYC__))
+#include <malloc.h>
+#include <string.h>
+#undef _s_realloc
+inline static void *_s_realloc(void *ptr, size_t size)
+{
+	void *ptr_next;
+	size_t size_prev = malloc_usable_size(ptr),
+	       size_next = size > size_prev ? size : size_prev;
+	ptr_next = malloc(size_next);
+	if (!ptr_next)
+		return NULL;
+	memcpy(ptr_next, ptr, size_prev);
+	free(ptr);
+	return ptr_next;
+}
+#endif
+
 #ifdef __cplusplus
 }      /* extern "C" { */
 #endif
